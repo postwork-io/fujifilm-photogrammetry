d93ace82c172afff8c049ce758605f1de2fc3c4d
Nathan Inglesby <nate@postwork.io>
2025-02-12 21:01:22 -0500

    added more accurate steps for polarizer

--- camera_control/settings.py
+++ camera_control/settings.py
@@ -6,6 +6,12 @@ TURNTABLE_STEPPER_PIN = int(os.getenv("TURNTABLE_STEPPER_PIN") or 16)
 POLARIZER_STEPPER_PIN = int(os.getenv("POLARIZER_STEPPER_PIN") or 22)
 POLARIZER_DIRECTION_PIN = int(os.getenv("POLARIZER_DIRECTION_PIN") or 32)
 TURNTABLE_STEPS_PER_ROTATION = int(os.getenv("TURNTABLE_STEPS_PER_ROTATION") or 800)
-POLARIZER_STEPS_PER_ROTATION = int(os.getenv("POLARIZER_STEPS_PER_ROTATION") or 800)
+POLARIZER_STEPS_PER_ROTATION = int(os.getenv("POLARIZER_STEPS_PER_ROTATION") or 960)
+"""
+Current Stepper Belt Gear has 20 Teeth
+Focus Ring has 96 teeth
+That gives us a gear ratio of 5/24 or 1:4.8
+Stepper steps is 200 for a total of 960 steps for a total rotation
+"""
 POST_PROCESS_URL = os.getenv("POST_PROCESS_URL") or "http://192.168.1.183:5000/upload"
 SETTLE_TIME = float(os.getenv("SETTLE_TIME", 3))
